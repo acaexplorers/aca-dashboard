1a52aaffbc639900c1caf76bc3f3390457e7a18d
Diana-hub4 <diana.cami.sar@icloud.com>
2025-06-09 15:06:25 -0500

    view profile user

--- src/app/views/user-profile/user-profile.component.ts
+++ src/app/views/user-profile/user-profile.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  physicalEnabled: boolean = true;
+  
+  sections = [
+    { name: 'SCHOLAR', enabled: true },
+    { name: 'MASTERY', enabled: true },
+    { name: 'GS', enabled: true },
+    { name: 'CLAN', enabled: true },
+    { name: 'HT', enabled: true },
+    { name: 'I+1', enabled: false }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
